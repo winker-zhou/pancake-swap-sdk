da8f8773248a8127a100c9f8c7e8313ecfdf818f
undefined <undefined>
2023-11-13 14:34:47 +0800

    ~

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | number | string
 
 export enum ChainId {
-  MAINNET = 56,
-  TESTNET = 97
+  MAINNET = 42161,
+  TESTNET = 421613
 }
 
 export enum TradeType {
